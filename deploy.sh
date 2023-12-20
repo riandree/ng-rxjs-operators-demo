c2afb5ec37faed3fdbf97c7cbda02aa76b50bc33
Andre <andre.rieck@gmx.de>
2023-12-20 14:53:45 +0100

    removed old contents of /docs folder for deployment

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ git add docs/keepme.git
 git commit -am "removed old contents of /docs folder for deployment"
 git fetch origin
 git merge -X theirs origin main
-sed -i 's\<base href="/">\<base href="/ng-rxjs-operators-demo/">\g' ./docs/index.html
 npm ci
 ng build
 cp dist/ng-rxjs-operators-demo/browser/* docs
+sed -i 's\<base href="/">\<base href="/ng-rxjs-operators-demo/">\g' ./docs/index.html
 git add docs/*
 git commit -m "new deployment"
 git push --force origin deploy
