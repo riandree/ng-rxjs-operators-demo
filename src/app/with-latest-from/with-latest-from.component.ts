36a7697a54160085dfe748212693e5231d943065
Andre <andre.rieck@gmx.de>
2023-12-20 15:41:04 +0100

    withLatestFrom

--- src/app/with-latest-from/with-latest-from.component.ts
+++ src/app/with-latest-from/with-latest-from.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DemoHeaderComponent } from '../demo-header/demo-header.component';
+import { Subject, timer, withLatestFrom } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ConsoleService } from '../service/console.service';
 
 @Component({
   selector: 'rxd-with-latest-from',
   standalone: true,
-  imports: [],
-  templateUrl: './with-latest-from.component.html',
-  styleUrl: './with-latest-from.component.css'
+  imports: [ DemoHeaderComponent, FormsModule ],
+  templateUrl: './with-latest-from.component.html'
 })
 export class WithLatestFromComponent {
+
+  currentLastValue = "42";
+
+  values : string[] = [];
+
+  private lastValues$=new Subject<string>();
+
+  constructor(consoleService : ConsoleService) {
+    this.lastValues$.pipe(
+      takeUntilDestroyed()      
+    ).subscribe(v => {
+      this.currentLastValue = v;
+    });
+
+    timer(0,300).pipe(
+      takeUntilDestroyed(),
+      withLatestFrom(this.lastValues$)      
+    ).subscribe(v => {
+      consoleService.print(`(${v})  `);
+    });
+    this.lastValues$.next(this.currentLastValue);
+    this.values.push(this.currentLastValue);
+  } 
+
+  newLastValue() {
+    this.lastValues$.next(this.currentLastValue);
+    this.values.push(this.currentLastValue);
+  }
 
 }
