6e078ffd1b796812928c7395cda636f413f7c7c8
Andre <andre.rieck@gmx.de>
2023-12-20 12:24:45 +0100

    altered deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ touch docs/keepme.git
 git add docs/keepme.git
 git commit -am "removed old contents of /docs folder for deployment"
 git merge main
-ng ci
+npm ci
 ng build
 cp /dist/ng-rxjs-operators-demo/browser/* /docs
 git commit -am "new deployment"
-git push origin deploy
+git push --force origin deploy
