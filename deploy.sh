371de6ff1db62e14d923aef5be95bdfadb12919e
Andre <andre.rieck@gmx.de>
2023-12-20 12:35:33 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 set -x
 git rm docs/*
-touch docs/keepme.git
+mkdir docs && echo "keep this folder" > keepme.git
 git add docs/keepme.git
 git commit -am "removed old contents of /docs folder for deployment"
-git merge main
+git merge -X theirs main
 npm ci
 ng build
 cp /dist/ng-rxjs-operators-demo/browser/* /docs
